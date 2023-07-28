14b309ca8f5a8504fa546d7983dc094095047eae
Yu-Lun <chou522128@gmail.com>
2023-07-28 22:53:44 +0800

    [Refactor] GameTest.cs：add SetUp method and use TestCase attribute

--- Bowling/Bowling/GameTest.cs
+++ Bowling/Bowling/GameTest.cs
@@ -5,12 +5,25 @@ namespace Bowling
     [TestFixture]
     public class GameTest
     {
-        [Test]
-        public void A01_GutterGame()
+        private Game _game;
+
+        [SetUp]
+        public void SetUp()
         {
-            var game = new Game();
-            var actual = game.ShowResult("-- -- -- -- -- -- -- -- -- --");
-            actual.Should().Be(0);
+            _game = new Game();
+        }
+
+        [Test]
+        [TestCase("-- -- -- -- -- -- -- -- -- --", 0)]
+        public void A01_GutterGame(string input, int expected)
+        {
+            AssertResultShouldReturn(input, expected);
+        }
+
+        private void AssertResultShouldReturn(string input, int expected)
+        {
+            var actual = _game.ShowResult(input);
+            actual.Should().Be(expected);
         }
     }
 }
