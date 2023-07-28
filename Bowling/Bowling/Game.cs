f97ad1ae164bf7940e3dac8c398f889705a01886
Yu-Lun <chou522128@gmail.com>
2023-07-28 22:40:04 +0800

    [Red] GameTest.cs：add test case
    input：-- -- -- -- -- -- -- -- -- --
    output：0

---
+++ Bowling/Bowling/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace Bowling
+{
+    public class Game
+    {
+        public int ShowResult(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bowling/Bowling/GameTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+namespace Bowling
+{
+    [TestFixture]
+    public class GameTest
+    {
+        [Test]
+        public void A01_GutterGame()
+        {
+            var game = new Game();
+            var actual = game.ShowResult("-- -- -- -- -- -- -- -- -- --");
+            actual.Should().Be(0);
+        }
+    }
+}
