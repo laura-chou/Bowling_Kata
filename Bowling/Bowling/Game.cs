b36b58b97c22cc223ef05e408289979bfbb6c279
Yu-Lun <chou522128@gmail.com>
2023-07-30 21:15:00 +0800

    [Refactor] Game.cs：split input to get frame and get all rows score to calculate sum

--- Bowling/Bowling/Game.cs
+++ Bowling/Bowling/Game.cs
@@ -4,10 +4,20 @@
     {
         public int ShowResult(string input)
         {
+            var frame = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
             if (input[0] == '1')
             {
-                return 20;
+                var sum = 0;
+                foreach (var item in frame)
+                {
+                    var row1 = int.Parse(item[0].ToString());
+                    var row2 = int.Parse(item[1].ToString());
+
+                    sum += row1 + row2;
+                }
+                return sum;
             }
+
             return 0;
         }
     }
