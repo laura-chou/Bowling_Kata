d09b8b056ed8f80c1ccd27a8ea1a42d83e4cfa35
Yu-Lun <chou522128@gmail.com>
2023-07-30 21:24:20 +0800

    [Refactor] Game.cs：add symbolMapper variable to get row score

--- Bowling/Bowling/Game.cs
+++ Bowling/Bowling/Game.cs
@@ -5,20 +5,20 @@
         public int ShowResult(string input)
         {
             var frame = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
-            if (input[0] == '1')
+            var symbolMapper = new Dictionary<char, int>
             {
-                var sum = 0;
-                foreach (var item in frame)
-                {
-                    var row1 = int.Parse(item[0].ToString());
-                    var row2 = int.Parse(item[1].ToString());
+                { '-', 0 },
+                { '1', 1 }
+            };
 
-                    sum += row1 + row2;
-                }
-                return sum;
+            var sum = 0;
+
+            foreach (var item in frame)
+            {
+                sum += symbolMapper[item[0]] + symbolMapper[item[1]];
             }
 
-            return 0;
+            return sum;
         }
     }
 }
