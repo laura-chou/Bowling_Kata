84ea6c6a144cf2e92751be520adf5e640919fe54
Yu-Lun <chou522128@gmail.com>
2023-08-12 22:06:44 +0800

    [Refactor] Game.cs：extract CalculateScore method

--- Bowling/Bowling/Game.cs
+++ Bowling/Bowling/Game.cs
@@ -1,4 +1,6 @@
-﻿namespace Bowling
+﻿using System.ComponentModel.Design;
+
+namespace Bowling
 {
     public class Game
     {
@@ -8,24 +10,48 @@
         {
             var frame = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
 
-            if (input[0] == 'X')
-            {
-                return 300;
-            }
             var game = frame.Select(rolls => new Game
             {
                 Rolls = GetRolls(rolls)
-            });
-            var sum = game.Sum(rolls => rolls.Rolls.Sum(roll => roll.Pins));
+            }).ToList();
+
+            var sum = CalculateScore(game);
 
             return sum;
+        }
+
+        private int CalculateScore(List<Game> game)
+        {
+            var score = 0;
+            int index = 0;
+            foreach (var rolls in game)
+            {
+                foreach(var roll in rolls.Rolls)
+                {
+                    var totalRoll = rolls.Rolls.Count;
+                    score += roll.Pins;
+                    // 如果只有一球而且是Strike
+                    if (totalRoll == 1 && roll.Pins == 10 && index != 9)
+                    {
+                        // 加上下兩球
+                        score += game[index + 1].Rolls[0].Pins;
+                        score += (index != 8)
+                                ? game[index + 2].Rolls[0].Pins
+                                : game[index + 1].Rolls[1].Pins;
+                    }
+                }
+                index++;
+            }
+
+            return score;
         }
 
         private List<Roll> GetRolls(string rolls)
         {
             var symbolMapper = new Dictionary<string, int>
             {
-                { "-", 0 }
+                { "-", 0 },
+                { "X", 10}
             };
 
             var rollList = rolls.Select(roll => new Roll
