f66153b27d7ea09375ed203aafc70247e5b378e1
Yu-Lun <chou522128@gmail.com>
2023-09-29 23:49:34 +0800

    [Refactor] GameTest.cs：extract AssertResultShouldReturn,SetUp  method

--- Bowling/Bowling/GameTest.cs
+++ Bowling/Bowling/GameTest.cs
@@ -6,12 +6,25 @@ namespace Bowling
     [TestFixture]
     public class GameTest
     {
-        [Test]
-        public void A01_GutterGame() 
+        private Game _game;
+
+        [SetUp]
+        public void SetUp() 
         {
-            var game = new Game();
-            var actual = game.ShowResult("-- -- -- -- -- -- -- -- -- --");
-            actual.Should().Be(0);
+            _game = new Game();
+        }
+
+        [Test]
+        [TestCase("-- -- -- -- -- -- -- -- -- --", 0)]
+        public void A01_GutterGame(string frame, int expected)
+        {
+            AssertResultShouldReturn(frame, expected);
+        }
+
+        public void AssertResultShouldReturn(string frame, int expected)
+        {
+            var actual = _game.ShowResult(frame);
+            actual.Should().Be(expected);
         }
     }
 }
