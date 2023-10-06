378a2be890ef11308054ebee14d4c2ba3e70b9e1
Yu-Lun <chou522128@gmail.com>
2023-10-06 22:21:39 +0800

    [Refactor] Game.cs：check roll andcalculate bonus

--- Bowling/Bowling/Game.cs
+++ Bowling/Bowling/Game.cs
@@ -1,4 +1,6 @@
-﻿namespace Bowling
+﻿using System.Linq;
+
+namespace Bowling
 {
     public class Game
     {
@@ -10,14 +12,51 @@
             var score = 0;
             if (game[0].FirstRoll.Pins == 10)
             {
-                score = 300;
+                var index = 0;
+                game.ForEach(rolls =>
+                {
+                    score += GetFirstRollPins(rolls) + GetBonus(game, index) + GetSecondRollPins(rolls) + GetThirdRollPins(rolls);
+                    index++;
+                });
             }
             else
             {
-                game.Sum(rolls => score += rolls.FirstRoll.Pins + rolls.SecondRoll.Pins);
+                game.Sum(rolls => score += GetFirstRollPins(rolls) + GetSecondRollPins(rolls));
             }
 
             return score;
+        }
+
+        private int GetBonus(List<Rolls> game, int index)
+        {
+            var bonus = 0;
+            if (!HaveThisRoll(game[index].SecondRoll))
+            {
+                bonus += game[index + 1].FirstRoll.Pins;
+                #pragma warning disable CS8602 // Dereference of a possibly null reference.
+                bonus += HaveThisRoll(game[index + 1].SecondRoll) ? game[index + 1].SecondRoll.Pins : game[index + 2].FirstRoll.Pins;
+            }
+            return bonus;
+        }
+
+        private int GetFirstRollPins(Rolls rolls)
+        {
+            return rolls.FirstRoll.Pins;
+        }
+
+        private int GetSecondRollPins(Rolls rolls)
+        {
+            return HaveThisRoll(rolls.SecondRoll) ? rolls.SecondRoll.Pins : 0;
+        }
+
+        private int GetThirdRollPins(Rolls rolls)
+        {
+            return HaveThisRoll(rolls.ThirdRoll) ? rolls.ThirdRoll.Pins : 0;
+        }
+
+        private bool HaveThisRoll(Roll? roll)
+        {
+            return roll != null;
         }
     }
 }
