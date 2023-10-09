8cb1bc886d5d907a80625b3f16402eddfabcd22f
Yu-Lun <chou522128@gmail.com>
2023-10-09 22:20:02 +0800

    [Green] GameTest.cs - A04_OnlyOneStrike：add test case
    input：11 11 11 11 11 11 11 11 11 X26
    output：36

--- Bowling/Bowling/GameTest.cs
+++ Bowling/Bowling/GameTest.cs
@@ -40,6 +40,7 @@ namespace Bowling
         [Test]
         [TestCase("X 11 11 11 11 11 11 11 11 11", 30)]
         [TestCase("11 11 11 X 11 11 11 11 11 11", 30)]
+        [TestCase("11 11 11 11 11 11 11 11 11 X26", 36)]
         public void A04_OnlyOneStrike(string frame, int expected)
         {
             AssertResultShouldReturn(frame, expected);
