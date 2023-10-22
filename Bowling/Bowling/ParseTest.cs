85e7e43a7e67cb6919c8bd00df0b9f9e3393f6f4
Yu-Lun <chou522128@gmail.com>
2023-10-22 21:12:30 +0800

    [Refactor] ParseTest.cs：extract SetUp,AssetResultShouldReturn method

--- Bowling/Bowling/ParseTest.cs
+++ Bowling/Bowling/ParseTest.cs
@@ -6,11 +6,18 @@ namespace Bowling
     [TestFixture]
     public class ParseTest
     {
+        private Parse _parse;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _parse = new Parse();
+        }
+
         [Test]
         public void A01_ParserFrame()
         {
-            var parse = new Parse();
-            var actual = parse.Parser("-- -- -- -- -- -- -- -- -- --");
+            var input = "-- -- -- -- -- -- -- -- -- --";
             var expected = new List<Rolls>
             {
                 new Rolls { Roll1 = new Roll { Pins = 0 }, Roll2 = new Roll { Pins = 0 } },
@@ -24,6 +31,12 @@ namespace Bowling
                 new Rolls { Roll1 = new Roll { Pins = 0 }, Roll2 = new Roll { Pins = 0 } },
                 new Rolls { Roll1 = new Roll { Pins = 0 }, Roll2 = new Roll { Pins = 0 } },
             };
+            AssetResultShouldReturn(input, expected);
+        }
+
+        private void AssetResultShouldReturn(string input, List<Rolls> expected)
+        {
+            var actual = _parse.Parser(input);
             actual.Should().BeEquivalentTo(expected);
         }
     }
