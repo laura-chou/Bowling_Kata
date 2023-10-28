e96845e74e0ed64d9da44aad69612a2de09ed81e
Yu-Lun <chou522128@gmail.com>
2023-10-28 21:21:15 +0800

    [Green] BowlingGameTest .cs -A02_AllRollNotKnockedDownAllPins：add test case
    input：9- 9- 9- 9- 9- 9- 9- 9- 9- 9-
    output：90

--- Bowling/Bowling/BowlingGameTest.cs
+++ Bowling/Bowling/BowlingGameTest.cs
@@ -24,6 +24,7 @@ namespace Bowling
         
         [Test]
         [TestCase("11 11 11 11 11 11 11 11 11 11", 20)]
+        [TestCase("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90)]
         public void A02_AllRollNotKnockedDownAllPins(string frame, int expected)
         {
             AssertResultShouldReturn(frame, expected);
