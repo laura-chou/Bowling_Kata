784edb23f090b756849a7003337db024d22e94fe
Yu-Lun <chou522128@gmail.com>
2023-10-29 15:20:45 +0800

    [Green] BowlingGame.cs ：if roll2 is null then hard code return

--- Bowling/Bowling/src/BowlingGame.cs
+++ Bowling/Bowling/src/BowlingGame.cs
@@ -8,6 +8,11 @@
             var parse = new Parse();
             var game = parse.Parser(frame);
 
+            if (game[0].Roll2 == null)
+            {
+                return 300;
+            }
+
             var score = game.Sum(rolls => rolls.Roll1.Pins + rolls.Roll2.Pins);
 
             return score;
